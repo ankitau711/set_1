7056b51890197f8446a57e92087a75ac0175199f
ankitau711 <35430197+ankitau711@users.noreply.github.com>
2018-01-23 21:14:35 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -4,6 +4,11 @@ void main()
 	int n,s,a,t;
 	printf("Enter the no");
 	scanf("%d",&n);
+	if(n<0)
+	printf("Invalid Input");
+	else
+	{
+	
 	t=n;
 	while(t!=0)
 	{
@@ -12,7 +17,8 @@ void main()
 		t/=10;
 	}
 	if(a==n)
-	printf("no is armstrong");
+	printf("no is palindrome");
 	else
-	printf("no is not armstrong");
+	printf("no is not palindrome");
+}
 }
